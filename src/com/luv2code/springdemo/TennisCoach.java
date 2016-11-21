3b5f116b837d30d05d3e6436670033d0be56a92a
Luca Quatela <lucaqq@gmail.com>
2016-11-21 18:34:09 +0100

    remove explicit bean ID from tennisCoach class to use default bean ID

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach") // this is the annotation that tells Spring to register this bean
+@Component // this is the annotation that tells Spring to register this bean
 public class TennisCoach implements Coach {
 
 	@Override
