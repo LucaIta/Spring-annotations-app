4087c5ec3cab2ff1a17db743b20c577d5168c9c6
Luca Quatela <lucaqq@gmail.com>
2016-11-23 11:29:58 +0100

    create an autoWired setFortuneService() method in TennisCoach class to set the fortuneService

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -8,11 +8,21 @@ public class TennisCoach implements Coach {
 	
 	private FortuneService fortuneService;
 	
-	@Autowired // what I'm doing here is that I'm autowiring it instead of telling by ID what dependency to inject with the config file
-	public TennisCoach(FortuneService theFortuneService) {
+//	@Autowired // what I'm doing here is that I'm autowiring it instead of telling by ID what dependency to inject with the config file
+//	public TennisCoach(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
+
+	public TennisCoach() {
+		System.out.println(">> inside default constructor");
+	}
+	
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> inside the setFortuneService() method");
 		fortuneService = theFortuneService;
 	}
-
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backend volley";
